a7d3f52c820cf6f0f8e1689542636a63e727e9b6
JAKHONGIR DARMANOV <97786628+darmanovjakhongir@users.noreply.github.com>
2024-10-07 23:43:54 -0500

    Add files via upload

---
+++ src/components/Feed.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import "../css/Feed.css";
+import Card from "./Card";
+
+const Feed = ({ pokemons }) => {
+  return (
+    <section className="pokemon-feed">
+      {pokemons.map((pokemon) => (
+        <Link to={`/${pokemon.name}`} key={pokemon.name}>
+          <Card data={pokemon} />
+        </Link>
+      ))}
+    </section>
+  );
+};
+
+export default Feed;
