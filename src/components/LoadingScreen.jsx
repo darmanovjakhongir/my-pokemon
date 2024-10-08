3167e885d65e6c512cc0d1b068f95cf3a2eb8e3f
JAKHONGIR DARMANOV <97786628+darmanovjakhongir@users.noreply.github.com>
2024-10-07 23:42:39 -0500

    Delete src directory

--- src/components/LoadingScreen.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const LoadingScreen = () => {
-  return <div className="loadingScreen">Pokemons Loading...</div>;
-};
-
-export default LoadingScreen;
--- src/pages/ErrorScreen.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const ErrorScreen = () => {
-  return <div>Error Occurred</div>;
-};
-
-export default ErrorScreen;
